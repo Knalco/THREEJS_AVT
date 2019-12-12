22a4ae2b4a98f19e085078df26788866e7056359
Knalco <36607027+Knalco@users.noreply.github.com>
2019-12-12 01:34:58 +0000

    Fix merge problems(?)

--- Frogger3D_Main/js/MovableObject.js
+++ Frogger3D_Main/js/MovableObject.js
@@ -15,15 +15,10 @@ function MovableObject(position, mesh, basicMat, lambertMat, phongMat){
     // Create a copy of the object's direction that we can safely manipulate.
     var dir = new THREE.Vector3(this.direction.x, this.direction.y, this.direction.z);
 
+    console.log("speed: " + this.speed);
     this.mesh.position.add(dir.multiplyScalar(this.speed*delta));
-    this.speed = Math.max(Math.min(this.speed + this.acceleration*delta, this.maxSpeed), -this.maxSpeed);
-    if (this.acceleration == 0){
-      if (this.speed > 0) {
-        this.speed = Math.max(this.speed+this.drag*delta, 0);
-      } else {
-        this.speed = Math.min(this.speed-this.drag*delta, 0);
-      }
-    }
+    //this.speed = Math.max(Math.min(this.speed*delta, this.maxSpeed), -this.maxSpeed);
+
 
     // Rotate
     var zz = new THREE.Vector3(0,0,1);
